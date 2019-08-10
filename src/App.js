74a5452e161c572a43fe0d538bbbb52181a3843a
Joseph M <joemark1989@gmail.com>
2019-08-10 05:15:02 -0400

    Updated UI going to test flex later

--- src/App.js
+++ src/App.js
@@ -49,23 +49,25 @@ function Main() {
   })
   return (
     <div>
-      <animated.div className="mainImg" style={props} />
-      <h1 className="centered">Deck Docks & Things</h1>
-      <p className="lead-centered">Decks, docks, piers, bulkheads, lifts and more. We offer complete turn key projects as well as pile driving for the DIY or the marine carpenter.
+      <section>
+        <animated.div className="mainImg" style={props} />
+        <h1 className="centered">Deck Docks & Things</h1>
+        <p className="lead-centered">Decks, docks, piers, bulkheads, lifts and more. We offer complete turn key projects as well as pile driving for the DIY or the marine carpenter.
       Licensed and Insured CGC 1526414</p>
-      <p className="lead-centered-next">Now offering sales and installation of the revolutionary TideSlide Mooring Products and Systems. Check them out <a href="http://www.tideslide.com/" target="_blank" rel="noopener noreferrer"><span className="tideSlide">here!</span></a></p>
-      <div className="arrow">
-        <span></span>
-        <span></span>
-        <span></span>
-      </div>
+        <p className="lead-centered-next">Now offering sales and installation of the revolutionary TideSlide Mooring Products and Systems. Check them out <a href="http://www.tideslide.com/" target="_blank" rel="noopener noreferrer"><span className="tideSlide">here!</span></a></p>
+        <div className="arrow">
+          <span></span>
+          <span></span>
+          <span></span>
+        </div>
+      </section>
     </div>
   )
 }
 
 function About() {
   return (
-    <div></div>
+    <div className="section background"></div>
   )
 }
 
