bd6be72eb7bdbef32b824dd23f5d9f258ad39183
Joseph M <joemark1989@gmail.com>
2019-08-16 03:31:05 -0400

    Updated services keeping color scheme for now, font is good, created boostrap grid for responsiveness, added margins to text

--- src/App.js
+++ src/App.js
@@ -71,24 +71,44 @@ function Home() {
   )
 }
 
+
+
+/* Custom Decks and Docks
+There is no project too big or too small for us. We'll work hard to add your own touch of character to each build through our distinctive attention to quality and craftsmanship. We can build classy and stylish at the same time as rugged and durable. DD&T provides a variety of materials, patterns and colors to complement your home.
+
+Bulkheads and Seawalls
+Bulkheads and seawalls can be complicated structures and the experts at DD&T know the necessary steps it takes to ensure yours will be long-lasting and attractive. With the knowledge and equipment to install the advanced materials and technologies needed to produce a low-maintenance, highly-durable barrier that meets all your retention needs and more. 
+
+Boatlifts and Things
+From boatlifts, deck lifts, and personal water craft lifts to outdoor kitchens with BBQs and fire pits and more... If you can dream it, we can build it.
+
+*/
+
+
+
+
 // Used bootstrap grid and some inline margins.
 
 function Services() {
   return (
-    <div className="background">
+    <div className="background-color">
       <div className="container">
         <div className="row">
           <div className="col-12">
             <h1 className="text-center">Services</h1>
             <div class="header-line"></div>
           </div>
-          <div className="col-12-sm">
-            <h1 className="text-center mt-3">Working With A Budget?</h1>
-            <p className="mt-3">DD&T works to provide you a stress-free and affordable outdoor living space solution that checks all the boxes of your must-have list. From start to finish, we strive to provide complete customer satisfaction and an incredible space you can be proud of for many years to come. Contact us for a free consultation today!
-            No One Beats Our Custom Deck Installations.
-            Get a customized vinyl or wood deck and dock combination for a perfect waterfront solution.​</p>
-            <h1 className="text-center mt-3">Working With A Budget?</h1>
-            <p className="mt-3">Decks, docks, bulkheads, lifts, hoists and more, we cater to all your custom needs.​</p>
+          <div className="col-xl-6 mt-3">
+            <h2 className="text-center">Custom Decks and Docks</h2>
+            <p className="mt-3">There is no project too big or too small for us. We'll work hard to add your own touch of character to each build through our distinctive attention to quality and craftsmanship. We can build classy and stylish at the same time as rugged and durable. DD&T provides a variety of materials, patterns and colors to complement your home.</p>
+          </div>
+          <div className="col-xl-6 mt-3">
+            <h2 className="text-center mt-3">Boatlifts and More</h2>
+            <p className="mt-3">From boatlifts, deck lifts, and personal water craft lifts to outdoor kitchens with BBQs and fire pits and more... If you can dream it, we can build it.</p>
+          </div>
+          <div className="col-xl-12 mt-3">
+            <h2 className="text-center mt-3">Bulkheads and Seawalls</h2>
+            <p className="mt-3">Bulkheads and seawalls can be complicated structures and the experts at DD&T know the necessary steps it takes to ensure yours will be long-lasting and attractive. With the knowledge and equipment to install the advanced materials and technologies needed to produce a low-maintenance, highly-durable barrier that meets all your retention needs and more. </p>
           </div>
         </div>
       </div>
@@ -108,12 +128,12 @@ function Gallery() {
 
 function App() {
   return (
-    <div className="">
+    <div>
       <Nav />
       <Home />
       <Services />
       <Gallery />
-    </div >
+    </div>
   );
 }
 
